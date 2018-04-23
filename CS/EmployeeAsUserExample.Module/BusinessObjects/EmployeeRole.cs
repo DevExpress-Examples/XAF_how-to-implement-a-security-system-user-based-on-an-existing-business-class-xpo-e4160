138b3b42204d68ac1b0283d59d80704bac4fb2f6
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/EmployeeAsUserExample.Module/BusinessObjects/EmployeeRole.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.ExpressApp.Security.Strategy;
+using DevExpress.Xpo;
+using DevExpress.Persistent.Base;
+
+namespace EmployeeAsUserExample.Module.BusinessObjects {
+    [ImageName("BO_Role")]
+    public class EmployeeRole : SecuritySystemRoleBase {
+        public EmployeeRole(Session session)
+            : base(session) {
+        }
+        [Association("Employees-EmployeeRoles")]
+        public XPCollection<Employee> Employees {
+            get {
+                return GetCollection<Employee>("Employees");
+            }
+        }
+    }
+}
---
+++ CS/EmployeeAsUserExample.Module/BusinessObjects/EmployeeTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.Xpo;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.ExpressApp.SystemModule;
+
+namespace EmployeeAsUserExample.Module.BusinessObjects {
+    [DefaultClassOptions, ImageName("BO_Task")]
+    [ListViewFilter("All Tasks", "")]
+    [ListViewFilter("My Tasks", "[Owner.Oid] = CurrentUserId()")]
+    public class EmployeeTask : Task {
+        public EmployeeTask(Session session)
+            : base(session) { }
+        private Employee owner;
+        [Association("Employee-Task")]
+        public Employee Owner {
+            get { return owner; }
+            set { SetPropertyValue("Owner", ref owner, value); }
+        }
+    }
+}
